0ba14ef76de8c70f1a57a0636815d4365bcfdaea
Mamun-swe <mhmamun166009@gmail.com>
2022-07-16 14:58:20 +0600

    Removed build directory

--- build/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const auth_route_1 = require("./auth.route");
-exports.router = (0, express_1.Router)();
-exports.router.use("/auth", auth_route_1.authRouter);
