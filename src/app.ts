a0c1f650d06f6f1255ccf9a88b88b3e8d071e496
mamun-swe <mamun.swe.277@gmail.com>
2023-03-26 12:57:58 +0600

    Dockerzied implemented

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,26 @@
-import express, { Express, NextFunction, Response, Request } from "express"
-import cors from "cors"
-import path from "path"
-import morgan from "morgan"
-import helmet from "helmet"
-import dotenv from "dotenv"
-import nocache from "nocache"
-import bodyParser from "body-parser"
-import compression from "compression"
-dotenv.config()
-import { router } from "./routes"
-import { errorHandeller } from "./middlewares/error-handeller.middleware"
-import jsonData from "../data.json"
+import express, { Express, NextFunction, Response, Request } from "express";
+import cors from "cors";
+import path from "path";
+import morgan from "morgan";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import nocache from "nocache";
+import bodyParser from "body-parser";
+import compression from "compression";
+dotenv.config();
+import { router } from "./routes";
+import { errorHandeller } from "./middlewares/error-handeller.middleware";
+import jsonData from "./json/api-endpoint.json";
 
-export const app: Express = express()
-app.use(cors())
-app.use(helmet())
-app.use(nocache())
-app.use(morgan('dev'))
-app.use(compression())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("views"))
+export const app: Express = express();
+app.use(cors());
+app.use(helmet());
+app.use(nocache());
+app.use(morgan("dev"));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("views"));
 
 /* set the view engine to ejs */
 app.set("views", path.join(__dirname, "../views"));
@@ -28,16 +28,16 @@ app.set("view engine", "ejs");
 
 /* Base route */
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.render("pages/index", { data: jsonData })
-})
+  res.render("pages/index", { data: jsonData });
+});
 
 /* Documentation route */
 app.get("/docs", (req: Request, res: Response, next: NextFunction) => {
-    res.render("pages/docs", { data: jsonData })
-})
+  res.render("pages/docs", { data: jsonData });
+});
 
 /* Integrate API routes */
-app.use("/api/v1", router)
+app.use("/api/v1", router);
 
 /* Error handelling middleware registration */
-app.use(errorHandeller)
+app.use(errorHandeller);
